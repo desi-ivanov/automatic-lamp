badac90375b658c31ef8f2a52e800948c56ee476
Desislav Ivanov <udie4we@abv.bg>
2021-11-07 14:09:01 +0100

    Lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 import * as functions from "firebase-functions";
-require("dotenv").config();
 import fetch from "node-fetch";
 import * as FormData from "form-data";
 import { firestore, initializeApp } from "firebase-admin";
@@ -20,21 +19,20 @@ if(!unito.st123456 || !unito.password || !tg.tg_bot_key) {
 
 initializeApp();
 
-
-const chatIDS = ["-1001253203905"]
+const chatIDS = ["-1001253203905"];
 const toBase64 = (s: string): string => Buffer.from(s).toString("base64");
 const basicAuth = (user: string, pass: string) => "Basic " + toBase64(user + ":" + pass);
 const authFetch = (url: string) => fetch(url, { headers: { Authorization: basicAuth(unito.st123456, unito.password) } });
 
-export const check = functions.pubsub.schedule('every minute').onRun(async () => {
+export const check = functions.pubsub.schedule("every minute").onRun(async () => {
   try {
-    const res = await authFetch(`https://wwwold.educ.di.unito.it/OffertaTesi/index.php`);
+    const res = await authFetch("https://wwwold.educ.di.unito.it/OffertaTesi/index.php");
     if(!res.ok) {
       throw new Error("Error while fetching: " + res.statusText + ". Status: " + res.status);
     }
     const resText = await res.text();
     const matches: Entry[] = (resText.match(/getDoc.+?<\//g) ?? [])
-      .map(plain => [plain, plain.match(/\d+/)?.[0]] as [string, string | undefined])
+      .map((plain) => [plain, plain.match(/\d+/)?.[0]] as [string, string | undefined])
       .filter((x): x is [string, string] => x[1] !== undefined)
       .map(([plain, id]) => ({
         title: plain.substr(plain.indexOf(">") + 1, plain.lastIndexOf("<") - plain.lastIndexOf(">") - 1),
@@ -50,8 +48,8 @@ export const check = functions.pubsub.schedule('every minute').onRun(async () =>
 
 async function sendUnseen(entries: Entry[]) {
   const all = firestore().collection("offerte_stage").doc("all");
-  const memo: { ids: string[] } = (await all.get()).data() as any ?? { ids: entries.map(e => e.id) };
-  for(let entry of entries) {
+  const memo = ((await all.get()).data() ?? { ids: entries.map((e) => e.id) }) as { ids: string[] };
+  for(const entry of entries) {
     try {
       if(!memo.ids.includes(entry.id)) {
         const sha256 = await sendEntry(entry);
@@ -71,7 +69,7 @@ async function sendEntry(entry: Entry): Promise<{ sha256: string }> {
     throw new Error(await entryDocument.text());
   }
   const buffer = await entryDocument.buffer();
-  for(let chatId of chatIDS) {
+  for(const chatId of chatIDS) {
     await sendMultimedia(
       "Offerta stage **" + entry.sede + "**: \n" + entry.title + "\n" + entry.url,
       buffer,
@@ -80,7 +78,7 @@ async function sendEntry(entry: Entry): Promise<{ sha256: string }> {
     );
   }
   return {
-    sha256: crypto.createHash("sha256").update(buffer).digest('hex')
+    sha256: crypto.createHash("sha256").update(buffer).digest("hex"),
   };
 }
 
@@ -90,7 +88,7 @@ async function sendMultimedia(message: string, document: Buffer, filename: strin
   form.append("document", document, { filename });
   console.log("sending");
   const res = await fetch(`https://api.telegram.org/${tg.tg_bot_key}/sendDocument?chat_id=${chatId}&parse_mode=markdown`, { method: "POST", body: form });
-  console.log("Sent", await res.text())
+  console.log("Sent", await res.text());
   if(!res.ok) {
     throw new Error(await res.text());
   }
